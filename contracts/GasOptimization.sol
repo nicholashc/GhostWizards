36ac4f74d3f2b370401c80ecf2e0e8ced09f2642
nicholashc <44581646+nicholashc@users.noreply.github.com>
2019-08-21 17:23:27 -0700

    fixed some comments on int/uint efficiency

--- contracts/GasOptimization.sol
+++ contracts/GasOptimization.sol
@@ -2,12 +2,14 @@ pragma solidity 0.5.11;
 
 // tests for most gas efficient way to flip a uint to the equiviliant negative int value
 // they were all tested in remix 0.5.11 with optimizer off and optimizer on at 200 cycles
-// they are the only functions in the contract because the evm adds 20 gas for r each new function or external variable ... 😡
+// they are the only functions in each contract because the evm adds 20 gas for each additional function or external variable ... 😡
 // splitting hairs on gas differences here so probabaly wasn't worth the effort
+// ranked in order off optimized gas execuition cost, then unoptimized
+// can also test wider range of inputs
 
 contract A {
     //221 gas optimized 0.5.11, (1000000) input, as only function in contract
-	//284 gas unoptimized 0.5.11, (1000000) input, as only function in contract
+    //284 gas unoptimized 0.5.11, (1000000) input, as only function in contract
     function flip(uint256 _num) public pure returns(int256) {
         return int256(0 - _num);
     }
@@ -66,7 +68,6 @@ contract H {
     //224 gas unoptimized 0.5.11, (1000000) input, as only function in contract
     //287 gas unoptimized 0.5.11, (1000000) input, as only function in contract
     function flip(uint256 _num) public pure returns(int256 res) {
-        
         assembly {
             res := add(not(_num), 1)
         }
@@ -112,4 +113,4 @@ contract K {
 }
 
 //other ideas:
-//using first bit as signuature for sigpos/neg
+//appending first bit as signuature for pos/neg sig and writing custom methods to read that
